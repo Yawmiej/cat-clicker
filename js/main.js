68319a3702bfe313d0370e876ed4a4953dbb66b1
Abdullateef <yomlateef@yahoo.com>
2019-01-16 03:22:21 +0100

    Added one more cat

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-let catImage = document.querySelector('#clickCat'),
-clickCount = 0,
-clicks = document.querySelector('#clicks');
-catImage.addEventListener('click', () => {
-    clickCount++;
-    clicks.innerHTML = `You clicked the cat image ${clickCount} time${clickCount === 1 ? '' : 's'}`;
-});
+let catImages = document.querySelectorAll('.clickCat'),
+clickCount1 = 0, clickCount2 = 0;
+clicks = document.querySelectorAll('.clicks');
+for (let catImage of catImages ) {
+    catImage.addEventListener('click', (evt) => {
+        if(evt.target === catImages[0]){
+            clickCount1++;
+            clicks[0].innerHTML = `You clicked the Jerry ${clickCount1} time${clickCount1 === 1 ? '' : 's'}`;
+        } else if(evt.target === catImages[1]) {
+            clickCount2++;
+            clicks[1].innerHTML = `You clicked the Teddy ${clickCount2} time${clickCount2 === 1 ? '' : 's'}`;
+        }
+        console.log(evt.target)
+    });
+}
